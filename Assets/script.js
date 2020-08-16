60ccd1fa9d44119b4470c26ea9104be58b6ddae4
naywilkins512 <nathanwilkins01@gmail.com>
2020-08-16 17:41:47 -0500

    adds styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,19 @@
 
 function getHeaderDate() {
-let currentDate = moment().format('MMMM Do YYYY');
-$("#currentDay").text(currentDate)
+    let currentDate = moment().format('MMMM Do YYYY');
+    $("#currentDay").text(currentDate)
 }
 
 getHeaderDate();
+
+
+
+$("#9amform").submit(function (e) {
+    e.preventDefault();
+    $("#9amtext").val
+    alert("save is working")
+});
+
+$(".btn-warning").click(function () {
+    alert("clear is working")
+});
