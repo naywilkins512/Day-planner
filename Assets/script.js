c2ef70a82fdcc92d85e78b709efc575d19bebb9b
naywilkins512 <nathanwilkins01@gmail.com>
2020-08-16 20:25:40 -0500

    adds partial local storage functionality

--- Assets/script.js
+++ Assets/script.js
@@ -8,20 +8,39 @@ getHeaderDate();
 
 
 
+let retrieveData = JSON.parse(localStorage.getItem("taskArr"))
 
-// $(".form-control").each(function())
-// submit(function (e) {
-//     e.preventDefault();
-//     $("i").val
-//     alert("save is working")
-// });
+
+let taskArr = []
+
+
+// $("#form-control").each(function() {
+
+$(".save-button").on("click", function(event) {
+    event.preventDefault();
+
+let hour = $(this).data("hour");
+let task = $("#" + hour + "text").val();
+taskArr.push(hour, task);
+localStorage.setItem("taskArr", JSON.stringify(taskArr));
+
 
 $(".btn-warning").click(function () {
-    alert("clear is working")
+    $("#9text").text("")
+
+})
+
+console.log(taskArr);
 });
+  
+// });
+
+
+
+
 
 
 //In terms of your question regarding the tasks, that’s really your call how you want to approach it. Remember you can use classes to target multiple elements, so one approach would be to ensure your textarea’s share a common class and some unique identifier and from there you can use jQuery target the class and loop over each element and write your logic to deal with the task
 
 
-// In a nutshell, yes. The class would be for the selector that you’ll use to loop over the elements and the id would be used the identify the timeslot.
+// In a nutshell, yes. The class would be for the selector that you’ll use to loop over the elements and the id would be used the identify the timeslot
